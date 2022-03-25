edebde1ddc0e2adb116c98beda14a89a7221d199
kth616 <nitraid.0106@gmail.com>
2022-03-25 18:35:53 +0900

    PHPでクイズアプリの実装

---
+++ template/404.tpl.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/questions.js" defer></script>
+    <title>問題<?php echo $id; ?> | Quiz</title>
+</head>
+<body>
+    <div id="main" class="container">
+        <h1>Quiz</h1>
+        問題は見つかりませんでした。
+        <div class="section">
+            <a href="index.html">戻る</a>
+        </div>
+    </div>
+    
+</body>
+</html>
